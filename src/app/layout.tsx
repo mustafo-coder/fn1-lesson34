8ff885ad4c2830815c64e45f2e292a2ca95cd520
mustafo-coder <shaeduloffdev@gmail.com>
2025-01-10 18:49:38 +0500

    fix problems

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import FooterWrapper from "./FooterWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} flex-col flex ${geistMono.variable} antialiased h-full`}
       >
-        <div className="flex-1">
-          <Navbar />
-          {children}
-        </div>
-        <FooterWrapper />
+        <div className="flex-1">{children}</div>
       </body>
     </html>
   );
